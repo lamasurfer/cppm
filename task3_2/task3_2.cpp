b891a414a4c6bc8e58c73df0b8f25c71ddfc0a84
lamasurfer <7376771@gmail.com>
2023-09-19 10:22:42 +0300

    added task3_2

---
+++ task3_2/task3_2.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <string>
+#include <regex>
+#include <Windows.h>
+
+class Counter {
+private:
+	int value;
+public:
+	Counter() { value = 1; }
+	Counter(int start) { value = start; }
+	int get_value() { return value; }
+	void set_value(int value) { this->value = value; }
+	int increment() { return ++value; }
+	int decrement() { return --value; }
+};
+
+int processIntInput();
+
+int main() {
+	setlocale(LC_ALL, "rus");
+
+	std::string input = "";
+	std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+
+	Counter counter;
+
+	std::getline(std::cin, input);
+	if (input == "да") {
+		std::cout << "Введите начальное значение счётчика: ";
+		int value = processIntInput();
+		counter.set_value(value);
+	}
+
+	do {
+		std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+		std::getline(std::cin, input);
+		if (input == "+") {
+			counter.increment();
+		}
+		else if (input == "-") {
+			counter.decrement();
+		}
+		else if (input == "=") {
+			std::cout << counter.get_value() << '\n';
+		}
+	} while (input != "x");
+
+	std::cout << "До свидания!\n";
+
+	return 0;
+}
+
+int processIntInput() {
+	std::string input;
+	std::regex longRegex("\\d+");
+	std::smatch match;
+	int result = 0;
+	while (std::getline(std::cin, input)) {
+		if (std::regex_match(input, match, longRegex)) {
+			try {
+				result = std::stoi(input);
+				break;
+			}
+			catch (std::out_of_range const& e) {
+				std::cout << "Вы ввели слишком большое число\n";
+			}
+		}
+		else {
+			std::cout << "Введите число, например 1000\n";
+		}
+	}
+	return result;
+}
